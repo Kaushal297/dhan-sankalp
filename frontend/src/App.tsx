825937ed5bd4dd2dd9b4ad53f9e4644f0c9ca18b
Kaushal Kumar <Kaushalkumar297@gmail.com>
2024-04-08 21:36:24 +0530

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './components/common/Footer'
+import Header from './components/common/Header'
+
+function App() {
+
+	return (
+		<>
+			<Header></Header>
+			<Footer></Footer>
+		</>
+	)
+}
+
+
+export default App
